df8afc5dc8abd41bee8283746fcd43cad3ff5ce2
imesh <imesh.nimsitha@gmail.ca>
2024-12-14 00:33:55 -0500

    updated comments

--- bloom_filter.py
+++ bloom_filter.py
@@ -6,7 +6,7 @@ class BloomFilter:
         """
         Initialize Bloom Filter
         """
-        self.size = size
+        self.size = size  # Initialize parameters
         self.num_hashes = num_hashes
         self.bit_array = bitarray.bitarray(size)
         self.bit_array.setall(0)
@@ -16,14 +16,14 @@ class BloomFilter:
         Generate hash for item using SHA-256
         """
         hash_input = f"{seed}{item}".encode('utf-8')
-        return int(hashlib.sha256(hash_input).hexdigest(), 16) % self.size
+        return int(hashlib.sha256(hash_input).hexdigest(), 16) % self.size  # Return hash value
     
     def insert(self, item):
         """
         Insert item into Bloom Filter
         """
         for i in range(self.num_hashes):
-            index = self._hash(item, i)
+            index = self._hash(item, i)  # Using hash function to get index
             self.bit_array[index] = True
     
     def __contains__(self, item):
